4d1257fc50bfdced346ea231cef8eff67f0fafb4
Makarovks320 <kirill.makarov@tafs.pro>
2022-11-15 21:35:30 +0300

    fix date

--- src/makeVideoItem.ts
+++ src/makeVideoItem.ts
@@ -1,14 +1,19 @@
 import {video} from "./index";
 
 export function makeVideoItem (v: video): video {
+    const ms = (new Date).getTime();
+    const createdDate = new Date(ms);
+    const D = new Date(ms);
+    const publicatedDate = new Date(D.setDate(D.getDate() + 1));
+
     return {
         id: new Date().valueOf(),
         title: v.title,
         author: v.author,
         canBeDownloaded: v.canBeDownloaded || false,
         minAgeRestriction: v.minAgeRestriction || null,
-        createdAt: v.createdAt || (new Date).toISOString(),
-        publicationDate: v.publicationDate || (new Date).toISOString(),
+        createdAt: v.createdAt || createdDate.toISOString(),
+        publicationDate: v.publicationDate || publicatedDate.toISOString(),
         availableResolutions: v.availableResolutions || null
     };
 }
